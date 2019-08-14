8ca3788a51093325f97fb568f7b4bf1a9256d3f9
katriyna <katriyna@gmail.com>
2019-08-14 17:17:25 +0300

    JT-53798 uncomment content of translations.js

--- src/app/translations.js
+++ src/app/translations.js
@@ -1,13 +1,13 @@
-// import de from './translations/youtrack-issues-list-widget_de.po';
-// import es from './translations/youtrack-issues-list-widget_es.po';
-// import fr from './translations/youtrack-issues-list-widget_fr.po';
-// import ru from './translations/youtrack-issues-list-widget_ru.po';
-// import ja from './translations/youtrack-issues-list-widget_ja.po';
+import de from './translations/youtrack-work-item-exporter-widget_de.po';
+import es from './translations/youtrack-work-item-exporter-widget_es.po';
+import fr from './translations/youtrack-work-item-exporter-widget_fr.po';
+import ru from './translations/youtrack-work-item-exporter-widget_ru.po';
+import ja from './translations/youtrack-work-item-exporter-widget_ja.po';
 
 export default {
-  // de: de.de_DE,
-  // es: es.es_ES,
-  // fr: fr.fr_FR,
-  // ru: ru.ru_RU,
-  // ja: ja.ja_JP
+  de: de.de_DE,
+  es: es.es_ES,
+  fr: fr.fr_FR,
+  ru: ru.ru_RU,
+  ja: ja.ja_JP
 };
