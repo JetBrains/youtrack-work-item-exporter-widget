426347b716943b8048976990554119f6cacdf70a
katriyna <katriyna@gmail.com>
2020-11-10 03:58:18 +0300

    supported custom translations + moved undo JetBrains organization

--- src/app/app.js
+++ src/app/app.js
@@ -4,16 +4,15 @@ import 'hub-dashboard-addons/dashboard.css';
 import React from 'react';
 import {render} from 'react-dom';
 import DashboardAddons from 'hub-dashboard-addons';
-import {setLocale} from 'hub-dashboard-addons/dist/localization';
 import ConfigWrapper from '@jetbrains/hub-widget-ui/dist/config-wrapper';
 
 import WorkItemsWidget from './work-items-widget';
-import TRANSLATIONS from './translations';
+import {initTranslations} from './translations';
 
 const CONFIG_FIELDS = ['filter'];
 
 DashboardAddons.registerWidget(async (dashboardApi, registerWidgetApi) => {
-  setLocale(DashboardAddons.locale, TRANSLATIONS);
+  initTranslations(DashboardAddons.locale);
   const configWrapper = new ConfigWrapper(dashboardApi, CONFIG_FIELDS);
 
   render(
--- src/app/translations.js
+++ src/app/translations.js
@@ -1,13 +1,22 @@
-import de from './translations/youtrack-work-item-exporter-widget_de.po';
-import es from './translations/youtrack-work-item-exporter-widget_es.po';
-import fr from './translations/youtrack-work-item-exporter-widget_fr.po';
-import ru from './translations/youtrack-work-item-exporter-widget_ru.po';
-import ja from './translations/youtrack-work-item-exporter-widget_ja.po';
+import {setLocale} from 'hub-dashboard-addons/dist/localization';
 
-export default {
-  de: de.de_DE,
-  es: es.es_ES,
-  fr: fr.fr_FR,
-  ru: ru.ru_RU,
-  ja: ja.ja_JP
-};
+const translationFiles = require.context('./translations/', true, /\.po$/);
+
+const translations = translationFiles.keys().
+  reduce((result, fileKey) => {
+    const lang = fileKey.split('.po')[0].split('_')[1];
+    const fileJson = translationFiles(fileKey);
+    result[lang] = Object.keys(fileJson).
+      reduce(
+        (accumulator, propertyKey) =>
+          ({...accumulator, ...fileJson[propertyKey]}),
+        {}
+      );
+    return result;
+  }, {});
+
+export function initTranslations(locale) {
+  if (translations[locale]) {
+    setLocale(locale, translations);
+  }
+}
